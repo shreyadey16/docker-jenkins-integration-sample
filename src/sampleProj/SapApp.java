a79a13d0ca81b2bd3d27a8eed89e5d1d4ba52fb5
unknown <shreya.dey1629@gmai.com>
2021-09-30 18:11:39 +0530

    first commit

---
+++ src/sampleProj/SapApp.java
@@ -0,0 +1,10 @@
+package sampleProj;
+
+public class SapApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello this is maven");
+	}
+
+}
